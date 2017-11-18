ae2f9dbdbd9010d3624f066400db040f5e727cb4
aslvrstn <aslvrstn@gmail.com>
2017-11-18 15:29:01 -0500

    Simulate games

---
+++ simulate_game.py
@@ -0,0 +1,19 @@
+import edpanalyst
+import pandas as pd
+
+ses = edpanalyst.Session()
+pm = ses.popmod('pm-psebx3z6beiciqgj')
+
+game = pm.simulate(given=pd.DataFrame({'ball': [str(b) for b in range(1, 6)]}), n=1)
+num_extra_balls = game['extra_balls'].astype(int).sum()
+num_extra_balls = min(num_extra_balls, 2)  # Cap this to 2 for now
+if num_extra_balls:
+  extra_balls = pm.simulate(given=pd.DataFrame({'ball': [str(b) for b in range(6, 6 + num_extra_balls)]}), n=1)
+  game = game.append(extra_balls)
+
+game = game.reset_index()
+del game['sim']
+del game['given_row']
+
+print(game)
+print(game['score'].sum())

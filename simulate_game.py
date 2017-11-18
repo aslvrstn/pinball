1a405081b9cc0af60297fa807502a6d96da3b421
aslvrstn <aslvrstn@gmail.com>
2017-11-18 18:05:01 -0500

    More data. Simulate a bunch of games'

--- simulate_game.py
+++ simulate_game.py
@@ -2,18 +2,24 @@ import edpanalyst
 import pandas as pd
 
 ses = edpanalyst.Session('aslvrstn')
-pm = ses.popmod('pm-qtytgktksflw4mat')
+pm = ses.popmod('pm-qwkiu3a3nf65qajn')
 
-game = pm.simulate(given=pd.DataFrame({'ball': [str(b) for b in range(1, 6)]}), n=1)
-num_extra_balls = game['extra_balls'].astype(int).sum()
-num_extra_balls = min(num_extra_balls, 2)  # Cap this to 2 for now
-if num_extra_balls:
-  extra_balls = pm.simulate(given=pd.DataFrame({'ball': [str(b) for b in range(6, 6 + num_extra_balls)]}), n=1)
-  game = game.append(extra_balls)
+def simulate_game():
+  game = pm.simulate(given=pd.DataFrame({'ball': [str(b) for b in range(1, 6)]}), n=1)
+  num_extra_balls = game['extra_balls'].astype(int).sum()
+  num_extra_balls = min(num_extra_balls, 2)  # Cap this to 2 for now
+  if num_extra_balls:
+    extra_balls = pm.simulate(given=pd.DataFrame({'ball': [str(b) for b in range(6, 6 + num_extra_balls)]}), n=1)
+    game = game.append(extra_balls)
 
-game = game.reset_index()
-del game['sim']
-del game['given_row']
+  game = game.reset_index()
+  del game['sim']
+  del game['given_row']
+  return game
 
-print(game)
-print(game['score'].sum())
+for _ in range(50):
+  game = simulate_game()
+  score = game['score'].sum()
+  if score > 1500000:
+    print(game)
+    print(score)

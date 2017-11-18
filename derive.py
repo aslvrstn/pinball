974e9acfc17f71c64dbf9a72944a13d959ffe555
aslvrstn <aslvrstn@gmail.com>
2017-11-18 13:47:02 -0500

    Script for deriving columns

---
+++ derive.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('pinball.csv')
+
+# Figure out the score per ball from the end score
+df['score'] = 0
+for i, row in df.iterrows():
+    prev = 0 if row['ball'] == 1 else df['score_at_end'][i-1]
+    df['score'][i] = row['score_at_end'] - prev
+
+# Wipe out our bonus if we tilted
+df['effective_bonus'] = df['bonus'] * ~df['tilt']
+df['effective_bonus_x'] = df['bonus_x'] * ~df['tilt']
+
+
+df.to_csv('derived_pinball.csv', index=False)
